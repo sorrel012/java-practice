f150e47afe8ebb997094025bd1598ec1e2851d38
hyowonhan <wain1719@gmail.com>
2023-01-20 16:33:40 +0900

    <feat> add java-loop

---
+++ review/src/Ex01Runner.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Ex01Runner {
+	public static void main(String[] args) {
+		Ex01 table = new Ex01();
+		table.print(3, 5, 10);
+
+		List<Integer> list = new ArrayList<>();
+
+	}
+
+}
