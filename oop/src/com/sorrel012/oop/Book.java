21e0e9418e4e6874247ab2b66c6054188981c5b6
HW <HW@DESKTOP-R7URR6K>
2023-01-22 23:32:58 +0900

    feat: create modifying nocopies method

--- oop/src/com/sorrel012/oop/Book.java
+++ oop/src/com/sorrel012/oop/Book.java
@@ -3,7 +3,19 @@ package com.sorrel012.oop;
 public class Book {
 	private int noOfCopies;
 
+	int getNoOfCopies() {
+		return this.noOfCopies;
+	}
+
 	void setNoOfCopies(int noOfCopies) { // local variable
 		this.noOfCopies = noOfCopies;
+	}
+
+	public void increaseCopies(int howMuch) {
+		this.noOfCopies += howMuch;
+	}
+
+	public void decreaseCopies(int howMuch) {
+		this.noOfCopies -= howMuch;
 	}
 }
--- oop/src/com/sorrel012/oop/BookRunner.java
+++ oop/src/com/sorrel012/oop/BookRunner.java
@@ -9,6 +9,20 @@ public class BookRunner {
 		artOfComputerProgramming.setNoOfCopies(100);
 		effactiveJava.setNoOfCopies(50);
 		cleanCode.setNoOfCopies(45);
+
+		artOfComputerProgramming.increaseCopies(30);
+		effactiveJava.increaseCopies(50);
+		cleanCode.increaseCopies(100);
+
+		artOfComputerProgramming.decreaseCopies(15);
+		effactiveJava.decreaseCopies(20);
+		cleanCode.decreaseCopies(40);
+
+		System.out.printf("artOfComputerProgramming's number of copies is %d\n",
+				artOfComputerProgramming.getNoOfCopies());
+		System.out.printf("effactiveJava's number of copies is %d\n", effactiveJava.getNoOfCopies());
+		System.out.printf("cleanCode's number of copies is %d\n", cleanCode.getNoOfCopies());
+
 	}
 
 }
