eeca232feec904f737b3fa5fda646b36901f2a7b
HW <HW@DESKTOP-R7URR6K.T-mobile.com>
2023-01-23 22:36:43 +0900

    feat: create default constructor

--- oop/src/com/sorrel012/oop/MotorBikeRunner.java
+++ oop/src/com/sorrel012/oop/MotorBikeRunner.java
@@ -4,6 +4,7 @@ public class MotorBikeRunner {
 	public static void main(String[] args) {
 		MotorBike ducati = new MotorBike(100);
 		MotorBike bmw = new MotorBike(200);
+		MotorBike honda = new MotorBike();
 
 		ducati.start();
 		bmw.start();
