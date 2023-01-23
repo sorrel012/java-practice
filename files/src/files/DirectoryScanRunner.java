c65bc34ff056059befc4d3fe019af117a1ff9c67
HW <HW@DESKTOP-R7URR6K.T-mobile.com>
2023-01-23 23:33:48 +0900

    feat: create file sorting method

--- files/src/files/DirectoryScanRunner.java
+++ files/src/files/DirectoryScanRunner.java
@@ -11,7 +11,12 @@ public class DirectoryScanRunner {
 
 //		Files.list(currentDirectory).forEach(System.out::println); //파일, 디렉토리 탐색
 
-		Files.walk(currentDirectory, 4).forEach(System.out::println); // 파일,디렉토리 깊이 설정하여 탐색
+//		Files.walk(currentDirectory, 4).forEach(System.out::println); // 파일,디렉토리 깊이 설정하여 탐색
 
+//		Files.walk(currentDirectory, 4).filter(path -> String.valueOf(path).contains(".java")) // 파일 골라내기
+//				.forEach(System.out::println);
+
+		Files.find(currentDirectory, 4, (path, attributes) -> String.valueOf(path).contains(".java"))// 파일 골라내기
+				.forEach(System.out::println);
 	}
 }
