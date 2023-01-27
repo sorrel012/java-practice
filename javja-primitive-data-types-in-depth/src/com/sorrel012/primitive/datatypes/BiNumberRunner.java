2fd5b292be7f40b52f2fe143fff8917eb09a303b
hyowonhan <wain1719@gmail.com>
2023-01-27 23:11:28 +0900

    feat: add Integer data type class

---
+++ javja-primitive-data-types-in-depth/src/com/sorrel012/primitive/datatypes/BiNumberRunner.java
@@ -0,0 +1,16 @@
+package com.sorrel012.primitive.datatypes;
+
+public class BiNumberRunner {
+	public static void main(String[] args) {
+		BiNumber numbers = new BiNumber(2, 3);
+
+		System.out.println(numbers.add());
+		System.out.println(numbers.multiply());
+
+		numbers.doubleValue();
+
+		System.out.println(numbers.getNumber1());
+		System.out.println(numbers.getNumber2());
+	}
+
+}
