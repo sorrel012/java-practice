0f50ace1e85a07686cbfd21639550464894768fa
hyowonhan <wain1719@gmail.com>
2023-01-31 00:26:24 +0900

    feat: create if statement practice class

---
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/IfStatementRunner.java
@@ -0,0 +1,14 @@
+package com.sorrel012.ifstatement.examples;
+
+public class IfStatementRunner {
+	public static void main(String[] args) {
+		int i = 23;
+
+		if (i == 25)
+			System.out.println("i = 25");
+		else if (i == 24)
+			System.out.println("i = 24");
+		else
+			System.out.println("i!= 25 and i != 24");
+	}
+}
