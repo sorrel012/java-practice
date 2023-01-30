291e1353d876d387cb73c26e498da1f5a7bcc84f
hyowonhan <wain1719@gmail.com>
2023-01-31 00:32:03 +0900

    feat: create if-else practice class

---
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/IfStatementRunner2.java
@@ -0,0 +1,35 @@
+package com.sorrel012.ifstatement.examples;
+
+public class IfStatementRunner2 {
+
+	public static void main(String[] args) {
+		puzzle1();
+		System.out.println();
+		puzzle2();
+		System.out.println();
+	}
+
+	private static void puzzle1() {
+		int k = 15;
+
+		if (k > 20)
+			System.out.println(1);
+		else if (k > 10)
+			System.out.println(2);
+		else if (k < 20)
+			System.out.println(3);
+		else
+			System.out.println(4);
+	}
+
+	private static void puzzle2() {
+		int l = 15;
+
+		if (l < 20)
+			System.out.println("l<20");
+		if (l > 20)
+			System.out.println("l>20");
+		else
+			System.out.println("Who am I?");
+	}
+}
