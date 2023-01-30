94ed6c6a75087da8fced097b55f073f5e5c0e5e4
hyowonhan <wain1719@gmail.com>
2023-01-31 00:33:26 +0900

    style: add block

--- conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/IfStatementRunner2.java
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/IfStatementRunner2.java
@@ -12,24 +12,27 @@ public class IfStatementRunner2 {
 	private static void puzzle1() {
 		int k = 15;
 
-		if (k > 20)
+		if (k > 20) {
 			System.out.println(1);
-		else if (k > 10)
+		} else if (k > 10) {
 			System.out.println(2);
-		else if (k < 20)
+		} else if (k < 20) {
 			System.out.println(3);
-		else
+		} else {
 			System.out.println(4);
+		}
 	}
 
 	private static void puzzle2() {
 		int l = 15;
 
-		if (l < 20)
+		if (l < 20) {
 			System.out.println("l<20");
-		if (l > 20)
+		}
+		if (l > 20) {
 			System.out.println("l>20");
-		else
+		} else {
 			System.out.println("Who am I?");
+		}
 	}
 }
