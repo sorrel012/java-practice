19e49122e3d443fd54cac028c2f551862624e602
hyowonhan <wain1719@gmail.com>
2023-01-31 21:40:20 +0900

    feat: create practice scanner(I) class

---
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/MenuRunner.java
@@ -0,0 +1,12 @@
+package com.sorrel012.ifstatement.examples;
+
+import java.util.Scanner;
+
+public class MenuRunner {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Number 1 : ");
+		int number1 = sc.nextInt();
+		System.out.println("The number tyou entered is :" + number1);
+	}
+}
