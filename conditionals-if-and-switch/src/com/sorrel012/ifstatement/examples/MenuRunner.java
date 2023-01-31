2f2a5b0f4aff54e402ed5bfdac54d827326a875e
hyowonhan <wain1719@gmail.com>
2023-01-31 21:42:04 +0900

    feat: add number2 method

--- conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/MenuRunner.java
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/MenuRunner.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class MenuRunner {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		System.out.print("Enter Number 1 : ");
 		int number1 = sc.nextInt();
-		System.out.println("The number tyou entered is :" + number1);
+		System.out.println("The first number you entered is :" + number1 + "\n");
+
+		System.out.print("Enter Number 2 : ");
+		int number2 = sc.nextInt();
+		System.out.println("The second number you entered is :" + number2);
 	}
 }
