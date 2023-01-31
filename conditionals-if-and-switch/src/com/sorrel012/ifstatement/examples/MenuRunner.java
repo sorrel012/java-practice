163af868c25c20ba5d8cb62a5caaf003df026774
hyowonhan <wain1719@gmail.com>
2023-01-31 21:48:59 +0900

    feat: add operation selecting method

--- conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/MenuRunner.java
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/MenuRunner.java
@@ -12,6 +12,28 @@ public class MenuRunner {
 
 		System.out.print("Enter Number 2 : ");
 		int number2 = sc.nextInt();
-		System.out.println("The second number you entered is :" + number2);
+		System.out.println("The second number you entered is :" + number2 + "\n");
+
+		System.out.println("1 - Add");
+		System.out.println("2 - Subtract");
+		System.out.println("3 - Divide");
+		System.out.println("4 - Multiply");
+		System.out.print("Chooses Operation number : ");
+		int operation = sc.nextInt();
+
+		int result = 0;
+
+		if (operation == 1) {
+			result = number1 + number2;
+		} else if (operation == 2) {
+			result = number1 - number2;
+		} else if (operation == 3) {
+			result = number1 * number2;
+		} else {
+			result = number1 / number2;
+		}
+
+		System.out.println("Result = " + result);
+
 	}
 }
