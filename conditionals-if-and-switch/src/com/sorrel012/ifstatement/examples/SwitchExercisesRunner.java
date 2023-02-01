db438a31fc1b88e2fc7f66c422768617a21eaf4b
hyowonhan <wain1719@gmail.com>
2023-02-01 23:38:45 +0900

    feat: create weekday validation method

--- conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/SwitchExercisesRunner.java
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/SwitchExercisesRunner.java
@@ -3,6 +3,29 @@ package com.sorrel012.ifstatement.examples;
 public class SwitchExercisesRunner {
 	public static void main(String[] args) {
 		System.out.println(determineNameOfDay(8));
+		System.out.println(isWeekDay(3));
+	}
+
+	public static boolean isWeekDay(int dayNumber) {
+		switch (dayNumber) {
+		case 0:
+			return false;
+		case 1:
+			return true;
+		case 2:
+			return true;
+		case 3:
+			return true;
+		case 4:
+			return true;
+		case 5:
+			return true;
+		case 6:
+			return false;
+		}
+
+		return false;
+
 	}
 
 	public static String determineNameOfDay(int dayNumber) {
