4430d4c39a7b9c68c76c8067febfd0a9c0579f02
hyowonhan <wain1719@gmail.com>
2023-02-01 23:21:08 +0900

    feat: add switch-case method

--- conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/MenuRunner.java
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/MenuRunner.java
@@ -8,21 +8,29 @@ public class MenuRunner {
 
 		System.out.print("Enter Number 1 : ");
 		int number1 = sc.nextInt();
-		System.out.println("The first number you entered is :" + number1 + "\n");
+		System.out.println("The first number you entered is : " + number1 + "\n");
 
 		System.out.print("Enter Number 2 : ");
 		int number2 = sc.nextInt();
-		System.out.println("The second number you entered is :" + number2 + "\n");
+		System.out.println("The second number you entered is : " + number2 + "\n");
 
 		System.out.println("1 - Add");
 		System.out.println("2 - Subtract");
-		System.out.println("3 - Divide");
-		System.out.println("4 - Multiply");
+		System.out.println("3 - Multiply");
+		System.out.println("4 - Divide");
 		System.out.print("Chooses Operation number : ");
 		int operator = sc.nextInt();
 
-		int result = 0;
+		int result1 = performOperationUsingNestedIfElse(number1, number2, operator);
+		int result2 = performOperationUsingSwitch(number1, number2, operator);
 
+		System.out.println("Result1 = " + result1);
+		System.out.println("Result2 = " + result2);
+
+	}
+
+	private static int performOperationUsingNestedIfElse(int number1, int number2, int operator) {
+		int result = 0;
 		if (operator == 1) {
 			result = number1 + number2;
 		} else if (operator == 2) {
@@ -32,8 +40,25 @@ public class MenuRunner {
 		} else {
 			result = number1 / number2;
 		}
+		return result;
+	}
 
-		System.out.println("Result = " + result);
-
+	private static int performOperationUsingSwitch(int number1, int number2, int operator) {
+		int result = 0;
+		switch (operator) {
+		case 1:
+			result = number1 + number2;
+			break;
+		case 2:
+			result = number1 - number2;
+			break;
+		case 3:
+			result = number1 * number2;
+			break;
+		default:
+			result = number1 / number2;
+			break;
+		}
+		return result;
 	}
 }
