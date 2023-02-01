3759a0c5f163b1ddb35c3f214914429e1d8ade23
hyowonhan <wain1719@gmail.com>
2023-02-01 23:39:50 +0900

    refactor: combine common case

--- conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/SwitchExercisesRunner.java
+++ conditionals-if-and-switch/src/com/sorrel012/ifstatement/examples/SwitchExercisesRunner.java
@@ -9,19 +9,14 @@ public class SwitchExercisesRunner {
 	public static boolean isWeekDay(int dayNumber) {
 		switch (dayNumber) {
 		case 0:
-			return false;
-		case 1:
-			return true;
-		case 2:
-			return true;
-		case 3:
-			return true;
-		case 4:
-			return true;
-		case 5:
-			return true;
 		case 6:
 			return false;
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			return true;
 		}
 
 		return false;
