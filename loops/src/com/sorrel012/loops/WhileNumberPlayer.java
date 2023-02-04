ee5197384bc44c58e25284b6083e794ecce2ff0c
hyowonhan <wain1719@gmail.com>
2023-02-04 16:51:16 +0900

    feat: create print squares method & print cubes method using while loop

---
+++ loops/src/com/sorrel012/loops/WhileNumberPlayer.java
@@ -0,0 +1,31 @@
+package com.sorrel012.loops;
+
+public class WhileNumberPlayer {
+    private int limit;
+
+    public WhileNumberPlayer(int limit) {
+	this.limit = limit;
+    }
+
+    public void printSquaresUptoLimit() {
+	int i = 1;
+
+	while(i*i <= limit) {
+	    System.out.print(i*i + " ");
+	    i++;
+	}
+	System.out.println();
+    }
+
+    public void printCubesuptoLimit() {
+	int i = 1;
+
+	while(i*i*i <= limit) {
+	    System.out.print(i*i*i + " ");
+	    i++;
+	}
+	System.out.println();
+    }
+
+
+}
---
+++ loops/src/com/sorrel012/loops/WhileNumberPlayerRunner.java
@@ -0,0 +1,12 @@
+package com.sorrel012.loops;
+
+public class WhileNumberPlayerRunner {
+    public static void main(String[] args) {
+
+	WhileNumberPlayer player = new WhileNumberPlayer(30);
+
+	player.printSquaresUptoLimit();
+
+	player.printCubesuptoLimit();
+    }
+}
