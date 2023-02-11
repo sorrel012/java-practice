98701d261e8dbe8adbb8752e086615c7c348019a
hyowonhan <wain1719@gmail.com>
2023-02-11 18:20:53 +0900

    feat: use variable variables

--- introduction-to-array-arraylist/src/com/sorreo012/arrays/Student.java
+++ introduction-to-array-arraylist/src/com/sorreo012/arrays/Student.java
@@ -8,7 +8,7 @@ public class Student {
     private String name;
     private int[] marks;
 
-    public Student(String name, int[] marks) {
+    public Student(String name, int... marks) {
 	this.name = name;
 	this.marks = marks;
     }
--- introduction-to-array-arraylist/src/com/sorreo012/arrays/StudentRunner.java
+++ introduction-to-array-arraylist/src/com/sorreo012/arrays/StudentRunner.java
@@ -6,9 +6,9 @@ public class StudentRunner {
 
     public static void main(String[] args) {
 
-	int[] marks = {97, 98, 100};
-
-	Student student = new Student("Hanee", marks);
+	Student student = new Student("Hanee", 97, 98, 100);
+	Student student1 = new Student("Sorrel", 97, 98);
+	Student student2 = new Student("Eve", 97, 98, 90, 98);
 
 	int number = student.getNumberOfMarks();
 	int sum = student.getTotalSumOfMarks();
