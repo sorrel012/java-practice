f749451fa342f7d251e66d2cf2bd17dc1ab4212b
hyowonhan <wain1719@gmail.com>
2023-02-13 22:51:06 +0900

    feat:; implements Comparable

--- collections/src/collections/StudnetsCollectionRunner.java
+++ collections/src/collections/StudnetsCollectionRunner.java
@@ -2,7 +2,17 @@ package collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class DescendingStudentComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student student1, Student student2) {
+	return Integer.compare(student2.getId(), student1.getId());
+    }
+
+}
 
 public class StudnetsCollectionRunner {
 
@@ -17,7 +27,15 @@ public class StudnetsCollectionRunner {
 	System.out.println(studentAl);
 
 	Collections.sort(studentAl);
-	System.out.println(studentAl);
+	System.out.println("오름차순: " + studentAl);
+
+	Collections.sort(studentAl, new DescendingStudentComparator());
+	System.out.println("내림차순: " + studentAl);
+
+	System.out.println();
+
+	studentAl.sort(new DescendingStudentComparator());
+	System.out.println("내림차순: " + studentAl);
 
     } //main
 }
