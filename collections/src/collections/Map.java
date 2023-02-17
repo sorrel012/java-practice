0d1648d324a54c9228aa03b4f4844e5f5ab68917
hyowonhan <wain1719@gmail.com>
2023-02-17 09:02:43 +0900

    feat: utilize treemap

--- collections/src/collections/Map.java
+++ collections/src/collections/Map.java
@@ -1,30 +1,34 @@
 package collections;
 
-import java.util.LinkedHashMap;
 import java.util.TreeMap;
 
 public class Map {
 
 	public static void main(String[] args) {
 
-		LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
-
-		linkedHashMap.put("F", 25);
-		linkedHashMap.put("A", 15);
-		linkedHashMap.put("Z", 5);
-		linkedHashMap.put("L", 250);
-
-		System.out.println(linkedHashMap);
-
 		TreeMap<String, Integer> treemap = new TreeMap<>();
 
 		treemap.put("F", 25);
-		treemap.put("A", 15);
 		treemap.put("Z", 5);
 		treemap.put("L", 250);
+		treemap.put("A", 15);
+		treemap.put("B", 25);
+		treemap.put("G", 25);
 
 		System.out.println(treemap);
 
+		System.out.println(treemap.higherKey("B"));
+		System.out.println(treemap.ceilingKey("B"));
+		System.out.println(treemap.lowerKey("B"));
+		System.out.println(treemap.floorKey("B"));
+		System.out.println();
+
+		System.out.println(treemap.firstEntry());
+		System.out.println(treemap.lastEntry());
+		System.out.println();
+
+		System.out.println(treemap.subMap("F", "Z"));
+		System.out.println(treemap.subMap("F", true, "Z", true));
 
 
 	} //main
