f78a6a7f8fe370c17216725df90f330891f6512f
hyowonhan <wain1719@gmail.com>
2023-02-18 22:17:57 +0900

    feat: modify excuting thread's numbers by using ExcutorService

--- Multithreading/src/ExecutorServiceRunner.java
+++ Multithreading/src/ExecutorServiceRunner.java
@@ -1,13 +1,37 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+class Task extends Thread {
+	
+	private int number;
+	
+	public Task(int number) {
+		this.number = number;
+	}
+	
+	@Override
+	public void run() { //SIGNATURE
+		System.out.print("\nTask" + number + " Started");
+		
+		for(int i= number*100; i <= number*100 + 99; i++) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.print("\nTask1 Done");
+	}
+	
+}
+
 public class ExecutorServiceRunner {
 
 	public static void main(String[] args) {
 
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
-		executorService.execute(new Task1());
-		executorService.execute(new Thread(new Task2()));
+//		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		executorService.execute(new Task(1));
+		executorService.execute(new Task(2));
+		executorService.execute(new Task(3));
+		executorService.execute(new Task(4));
 
 		//Task3
 		System.out.print("\nTask3 Kicked Off");
