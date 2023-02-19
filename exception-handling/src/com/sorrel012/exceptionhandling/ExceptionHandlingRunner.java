076bea9894e5487686653d05bf7ffc3ba163e0e1
hyowonhan <wain1719@gmail.com>
2023-02-19 13:17:47 +0900

    feat: create null pointer exception

---
+++ exception-handling/src/com/sorrel012/exceptionhandling/ExceptionHandlingRunner.java
@@ -0,0 +1,24 @@
+package com.sorrel012.exceptionhandling;
+
+public class ExceptionHandlingRunner {
+	
+	public static void main(String[] args) {
+
+		method1();
+		System.out.println("Main Ended");
+
+	} //main
+
+	private static void method1() {
+		method2();
+		System.out.println("Method1 Ended");
+	}
+	
+	private static void method2() {
+		String str = null;
+		str.length();
+		System.out.println("Method2 Ended");
+	}
+	
+	
+}
