cd09a4c69c0e580092d68f0a54d287483aa52aaa
sorrel012 <wain1719@gmail.com>
2023-04-13 21:30:13 +0900

    refactor: use Arrays.sort instead of nested for-loop

--- algorithm/src/com/sorrel012/sort/Q05.java
+++ algorithm/src/com/sorrel012/sort/Q05.java
@@ -3,6 +3,7 @@ package com.sorrel012.sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
   
 public class Q05 {
@@ -30,15 +31,13 @@ public class Q05 {
   
   private static String solution(int n, int[] arr) {
             
+      Arrays.sort(arr);
       for(int i = 0; i < n-1; i++) {
-          for(int j = i + 1; j < n; j++) {
-              
-              if(arr[i] == arr[j]) {
-                  return "D";
-              }
+          if(arr[i] == arr[i+1]) {
+              return "D";
           }
       }
-      
+
       return "U";
   }
 }
