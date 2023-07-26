ce04a85d0000b0959045e42c6f923caa64876acf
sorrel012 <wain1719@gmail.com>
2023-07-26 12:41:13 +0900

    feat: Create board form

--- springboot/board/src/main/java/com/study/board/controller/BoardController.java
+++ springboot/board/src/main/java/com/study/board/controller/BoardController.java
@@ -2,16 +2,24 @@ package com.study.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main() {
+    @GetMapping("/board/write") //localhost:8080/board/write
+    public String boardWriteForm() {
+        return "boardwrite";
+    }
 
-        return "Hello Word";
+    @PostMapping("/board/writepro")
+    public String boardWritePro(String title, String content) {
+
+        System.out.println("제목 : " + title);
+        System.out.println("내용 : " + content);
+
+        return "";
     }
 
 }
