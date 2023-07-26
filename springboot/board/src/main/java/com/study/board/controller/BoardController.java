ab21817ac82a6759a968db8d442c8b857d046f77
sorrel012 <wain1719@gmail.com>
2023-07-26 13:07:03 +0900

    feat: Create Board DTO

--- springboot/board/src/main/java/com/study/board/controller/BoardController.java
+++ springboot/board/src/main/java/com/study/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +15,7 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(String title, String content) {
-
-        System.out.println("제목 : " + title);
-        System.out.println("내용 : " + content);
+    public String boardWritePro(Board board) {
 
         return "";
     }
