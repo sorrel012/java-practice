c12efadc04c60ef9bf04c2e44013c069646c344b
sorrel012 <wain1719@gmail.com>
2023-07-26 13:14:44 +0900

    feat: Connect to Database using JPA

--- springboot/board/src/main/java/com/study/board/controller/BoardController.java
+++ springboot/board/src/main/java/com/study/board/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.study.board.controller;
 
 import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/board/write") //localhost:8080/board/write
     public String boardWriteForm() {
         return "boardwrite";
@@ -16,6 +21,8 @@ public class BoardController {
 
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board) {
+
+        boardService.write(board);
 
         return "";
     }
