b8838d6878b6baa21f2770e70ee7d771ccd1aa2f
sorrel012 <wain1719@gmail.com>
2023-07-27 18:00:58 +0900

    feat: Implement board view page

--- springboot/board/src/main/java/com/study/board/controller/BoardController.java
+++ springboot/board/src/main/java/com/study/board/controller/BoardController.java
@@ -36,4 +36,12 @@ public class BoardController {
         return "boardlist";
     }
 
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id) {
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardview";
+    }
+
 }
